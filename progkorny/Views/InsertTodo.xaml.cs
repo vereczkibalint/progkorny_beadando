7a84af041089558a117e0a3940982cf2a49366d0
vereczkibalint <vereczkibalint@gmail.com>
2019-11-12 11:07:51 +0100

    Added check if any of the textboxes is empty

--- progkorny/Views/InsertTodo.xaml.cs
+++ progkorny/Views/InsertTodo.xaml.cs
@@ -36,26 +36,33 @@ namespace progkorny
         /// <param name="e"></param>
         private void InsertBtn_Click(object sender, RoutedEventArgs e)
         {
-            // Todo adatai
-            Todo insertTodo = new Todo();
-            insertTodo.Todo_Title = txtBox_Title.Text;
-            insertTodo.Todo_Body = txtBox_Body.Text;
-            insertTodo.Todo_Author = txtBox_Author.Text;
-            insertTodo.Todo_Deadline = dateDeadline.Text;
-            insertTodo.Todo_Priority = (Priority)Enum.Parse(typeof(Priority), cmbPriority.SelectedValue.ToString().ToUpper());
-
-            // Az insert kísérlete
-            int insertResult = todoController.InsertTodo(insertTodo);
-            // -1 - Sikertelen insert
-            // 1 - Sikeres insert
-
-            if(insertResult == 1)
+            if (!TodoHelper.IsEmptyOrNull(txtBox_Title.Text, txtBox_Body.Text, txtBox_Author.Text, dateDeadline.Text))
             {
-                this.Close();
+                // Todo adatai
+                Todo insertTodo = new Todo();
+                insertTodo.Todo_Title = txtBox_Title.Text;
+                insertTodo.Todo_Body = txtBox_Body.Text;
+                insertTodo.Todo_Author = txtBox_Author.Text;
+                insertTodo.Todo_Deadline = dateDeadline.Text;
+                insertTodo.Todo_Priority = (Priority)Enum.Parse(typeof(Priority), cmbPriority.SelectedValue.ToString().ToUpper());
+
+                // Az insert kísérlete
+                int insertResult = todoController.InsertTodo(insertTodo);
+                // -1 - Sikertelen insert
+                // 1 - Sikeres insert
+
+                if (insertResult == 1)
+                {
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Hiba történt az adatfelvitel közben!");
+                }
             }
             else
             {
-                MessageBox.Show("Hiba történt az adatfelvitel közben!");
+                MessageBox.Show("Tölts ki minden mezőt!");
             }
         }
     }
